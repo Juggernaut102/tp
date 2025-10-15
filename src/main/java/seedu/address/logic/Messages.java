50ba0f50680db813303e36fc38da85d085416f3a
ryan-toh <ryan_toh@outlook.sg>
2025-10-15 13:13:41 +0800

    Fix message log bug

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -43,7 +43,7 @@ public class Messages {
                 .append(person.getEmail())
                 .append("; Address: ")
                 .append(person.getAddress())
-                .append(";")
+                .append("; Subject: ")
                 .append(person.getSubject())
                 .append("; Tags: ");
         person.getTags().forEach(builder::append);
