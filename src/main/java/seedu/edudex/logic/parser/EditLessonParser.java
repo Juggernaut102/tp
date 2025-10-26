cf5ee8d53d22c062a0d235522f49e4d50c6bfe25
Starishine <sania.amarnath@gmail.com>
2025-10-26 13:08:09 +0800

    Add edit lesson feature

---
+++ src/main/java/seedu/edudex/logic/parser/EditLessonParser.java
@@ -0,0 +1,35 @@
+package seedu.edudex.logic.parser;
+
+import static seedu.edudex.logic.parser.CliSyntax.PREFIX_DAY;
+import static seedu.edudex.logic.parser.CliSyntax.PREFIX_END;
+import static seedu.edudex.logic.parser.CliSyntax.PREFIX_START;
+import static seedu.edudex.logic.parser.CliSyntax.PREFIX_SUBJECT;
+
+import java.util.List;
+
+import seedu.edudex.logic.parser.exceptions.ParseException;
+import seedu.edudex.model.person.Lesson;
+
+public class EditLessonParser {
+
+    public EditLessonDescriptor parse(ArgumentMultimap argMultimap) throws ParseException {
+        EditLessonDescriptor editLessonDescriptor = new EditLessonDescriptor();
+
+        if (argMultimap.getValue(PREFIX_SUBJECT).isPresent()) {
+            editLessonDescriptor.setSubject(
+                    ParserUtil.parseSubjectName(argMultimap.getValue(PREFIX_SUBJECT).get()));
+        }
+        if (argMultimap.getValue(PREFIX_DAY).isPresent()) {
+            editLessonDescriptor.setDay(
+                    ParserUtil.parseDay(argMultimap.getValue(PREFIX_DAY).get()));
+        }
+        if (argMultimap.getValue(PREFIX_START).isPresent()) {
+            editLessonDescriptor.setStartTime(ParserUtil.parseTime(argMultimap.getValue(PREFIX_START).get()));
+        }
+        if (argMultimap.getValue(PREFIX_END).isPresent()) {
+            editLessonDescriptor.setEndTime(ParserUtil.parseTime(argMultimap.getValue(PREFIX_END).get()));
+        }
+
+        return editLessonDescriptor;
+    }
+}
