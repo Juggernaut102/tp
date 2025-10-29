0cea43d0b78e3520c54379f44732d506b7804c16
nghaowei <nghaoweiftw@gmail.com>
2025-10-29 11:48:44 +0800

    Remove debug output from PersonCard rendering
    Removed debug print statements for rendering PersonCard.

--- src/main/java/seedu/edudex/ui/PersonCard.java
+++ src/main/java/seedu/edudex/ui/PersonCard.java
@@ -58,8 +58,5 @@ public class PersonCard extends UiPart<Region> {
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
         subject.setText(person.getLessonsAsString());
-        System.out.println("Rendering PersonCard for: " + person.getName());
-        person.getLessons().forEach(lesson ->
-                System.out.println("  Lesson: " + lesson.getSubject().getSubjectAsString()));
     }
 }
