c66cf09601f268ef12e76aaedf841fa99829dad1
ryan-toh <ryan_toh@outlook.sg>
2025-10-31 09:54:03 +0800

    import, comment hotfixes

--- src/main/java/seedu/edudex/ui/SubjectCard.java
+++ src/main/java/seedu/edudex/ui/SubjectCard.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.Region;
 import seedu.edudex.model.subject.Subject;
 
 /**
- * An UI component that displays information of a {@code Person}.
+ * An UI component that displays information of a {@code Subject}.
  */
 public class SubjectCard extends UiPart<Region> {
 
@@ -31,7 +31,7 @@ public class SubjectCard extends UiPart<Region> {
     private Label id;
 
     /**
-     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     * Creates a {@code SubjectCode} with the given {@code Subject} and index to display.
      */
     public SubjectCard(Subject subject) {
         super(FXML);
@@ -40,7 +40,7 @@ public class SubjectCard extends UiPart<Region> {
     }
 
     /**
-     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     * Creates a {@code SubjectCode} with the given {@code Subject} and index to display.
      */
     public SubjectCard(Subject subject, int displayedIndex) {
         super(FXML);
