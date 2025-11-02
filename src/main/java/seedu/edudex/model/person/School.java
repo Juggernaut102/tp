e2a68568fab1b4c946c67067ab065534460c6446
ryan-toh <ryan_toh@outlook.sg>
2025-11-02 20:31:30 +0800

    update school regex (allow special char)

--- src/main/java/seedu/edudex/model/person/School.java
+++ src/main/java/seedu/edudex/model/person/School.java
@@ -9,13 +9,14 @@ import static seedu.edudex.commons.util.AppUtil.checkArgument;
  */
 public class School {
     public static final String MESSAGE_CONSTRAINTS =
-            "Schools should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Schools should only contain alphanumeric characters, special characters (except '/') and " +
+                    "spaces, and it should not be blank";
 
     /**
      * The first character of the school name must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "^[\\p{Alnum}\\p{Punct}&&[^/]][\\p{Alnum} \\p{Punct}&&[^/]]*$";
 
     public final String value;
 
